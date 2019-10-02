c62568c6b2cc524c869341f3f534bec8ddde7769
2656AK <55872444+2656AK@users.noreply.github.com>
2019-10-02 15:26:46 +0200

    Initial Chucknorrisjoke

---
+++ ChuckNorrisJokeSIn/ChuckNorrisJokes/Program.cs
@@ -0,0 +1,25 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Jokesgenerator jokeGenerator = new Jokesgenerator();
+            string randomJoke = await jokeGenerator.GetRandomJoke();
+
+            Console.WriteLine(randomJoke);
+
+            string[] catergories = await jokeGenerator.GetCategories();
+
+            for (int index = catergories.Length - 1; index >= 0; index--)
+            {
+                Console.WriteLine(catergories[index]);
+            }
+        }   
+                
+    }
+}
---
+++ ChuckNorrisJokeSIn/ChuckNorrisJokesLibrary/Jokesgenerator.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesLibrary
+{
+    public class Jokesgenerator
+    {
+        public async Task<string> GetRandomJoke()
+
+        {
+            HttpClient client = new HttpClient();
+            string randomString = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+
+            var joke = JsonConvert.DeserializeObject<Joke>(randomString);
+
+            return joke.value;
+        }
+
+
+        public async Task<string[]> GetCategories()
+        {
+            HttpClient client = new HttpClient();
+
+            string categoryJokeString = await client.GetStringAsync("https://api.chucknorris.io/jokes/categories");
+
+            var categories = JsonConvert.DeserializeObject<string[]>(categoryJokeString);
+
+            return categories;
+
+        }
+    }
+}
